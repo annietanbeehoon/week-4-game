57c2022eb55ede2af4c974bb69e10a87bcc59b72
Annie Schaefer <annie@Annies-MBP.fios-router.home>
2018-03-20 21:45:07 -0400

    First commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,17 @@
-$(document).ready(function () {
+    // create variable to record game tally and current total
+    var currentScore = 0;
+    var winTally = 0;
+    var lossTally = 0;
 
-    var randomNum = Math.floor(Math.random() * (121 - 19)) + 19;
-    $("#randomNumber").text(randomNum);
-    console.log(randomNum);
-
-    $("#target").click(function() {
-    var randomNum = Math.floor(Math.random() * (121 - 19)) + 19;
-    $("#randomNumber").text(randomNum);
-    console.log(randomNum);
-    reset();
-});
-
-    
+    //create a random number for player to match the number
     // for(var i = 0; i < 4; i++) {
     // var randomBookNumber = Math.floor(Math.random() * (13 - 1)) + 1;
     // console.log("This is the value of each random number " + randomBookNumber);
+    var randomNum = Math.floor(Math.random() * (121 - 19)) + 19;
+    $("#randomNumber").text(randomNum);
+    console.log(randomNum);
 
+    //create four random numbers within range to be assigned to each book
     book1 = Math.floor(Math.random() * (13 - 1)) + 1;
     console.log(book1);
     book2 = Math.floor(Math.random() * (13 - 1)) + 1;
@@ -25,74 +21,7 @@ $(document).ready(function () {
     book4 = Math.floor(Math.random() * (13 - 1)) + 1;
     console.log(book4);
 
-    // create variable to record game tally and current total
-    var currentScore = 0;
-    var winTally = 0;
-    var lossTally = 0;
-
-    $("#one").on('click', function () {
-        currentScore = currentScore + book1;
-        console.log("New currentscore=" + currentScore);
-        $('#finalTotal').text(currentScore);
-        if (currentScore === randomNum) {
-            win();
-        } else if (currentScore > randomNum) {
-            losses();
-        }
-    });
-    $("#two").on('click', function () {
-        currentScore = currentScore + book2;
-        console.log("New currentscore=" + currentScore);
-        $('#finalTotal').text(currentScore);
-        if (currentScore === randomNum) {
-            win();
-        } else if (currentScore > randomNum) {
-            losses();                        
-}
-    });
-    $("#three").on('click', function () {
-        currentScore = currentScore + book3;
-        console.log("New currentscore=" + currentScore);
-        $('#finalTotal').text(currentScore);
-        if (currentScore === randomNum) {
-            win();
-
-        } else if (currentScore > randomNum) {
-            losses();
-        }
-
-    });
-    $("#four").on('click', function () {
-        currentScore = currentScore + book4;
-        console.log("New currentscore=" + currentScore);
-        $('#finalTotal').text(currentScore);
-        if (currentScore === randomNum) {
-            win();
-
-        } else if (currentScore > randomNum) {
-            losses();
-        }
-    });
-    function win() {
-        winTally++;
-        $("#wins").html(winTally);
-        alert("You win!");
-        $("#finalTotal").text("");
-        currentScore = 0;
-    }
-
-    function losses() {
-        lossTally++;
-        $("#loss").html(lossTally);
-        alert("Your total score went over!");
-        currentScore=0;
-        $("#finalTotal").text("");
-        currentScore = 0;
-    }
-
-    // display in html to the player to show the count of win and loss
-    $("#wins").text(winTally);
-    $("#loss").text(lossTally);
+$(document).ready(function () {
 
     // reset the game
     function reset() {
@@ -104,8 +33,88 @@ $(document).ready(function () {
         console.log(book3);
         book4 = Math.floor(Math.random() * (13 - 1)) + 1;
         console.log(book4);
+        var randomNum = Math.floor(Math.random() * (121 - 19)) + 19;
         currentScore=0;
         $("#finalTotal").text("");
     }
+    $("#target").click(function() {
+        var randomNum = Math.floor(Math.random() * (121 - 19)) + 19;
+        $("#randomNumber").text(randomNum);
+        console.log(randomNum);
+        reset();
+    });
+    //create function to start on each click, accumulate the points and post it to the finalTotal for user to match to the random number
+
+    $("#one").on('click', function () {
+        currentScore = currentScore + book1;
+        console.log("New currentscore=" + currentScore);
+        $('#finalTotal').text(currentScore);
+        if (currentScore === randomNum) {
+            win();
+            reset()
+        } else if (currentScore > randomNum) {
+            losses();
+            reset()
+        }
+    });
+    $("#two").on('click', function () {
+        currentScore = currentScore + book2;
+        console.log("New currentscore=" + currentScore);
+        $('#finalTotal').text(currentScore);
+        if (currentScore === randomNum) {
+            win();
+            reset()
+        } else if (currentScore > randomNum) {
+            losses();
+            reset()                    
+    }
+    });
+    $("#three").on('click', function () {
+        currentScore = currentScore + book3;
+        console.log("New currentscore=" + currentScore);
+        $('#finalTotal').text(currentScore);
+        if (currentScore === randomNum) {
+            win();
+            reset()
+
+        } else if (currentScore > randomNum) {
+            losses();
+            reset()
+        }
+
+    });
+    $("#four").on('click', function () {
+        currentScore = currentScore + book4;
+        console.log("New currentscore=" + currentScore);
+        $('#finalTotal').text(currentScore);
+        if (currentScore === randomNum) {
+            win();
+            reset()
+        } else if (currentScore > randomNum) {
+            losses();
+            reset()
+        }
+    });
+
+    //Create a function for what happens after a win, add counter to winTally; input into html; alert a message; empty the finalTotal; set current score to zero.
+    function win() {
+        winTally++;
+        $("#wins").html(winTally);
+        alert("You win!");
+        $("#finalTotal").text("");
+        reset();
+    }
+
+    function losses() {
+        lossTally++;
+        $("#loss").html(lossTally);
+        alert("Your total score went over!");
+        $("#finalTotal").text("");
+        reset();
+    }
+
+    // display in html to the player to show the count of win and loss
+    $("#wins").text(winTally);
+    $("#loss").text(lossTally);
     
 });
